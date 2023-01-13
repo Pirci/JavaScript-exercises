4d7b51a267534e35cd3534d60d0ee15e4a1310e2
emre <epirci@megagrouptrade.com>
2023-01-13 07:59:25 +0100

    BMI calculation and compare

---
+++ markHigherBMI.js
@@ -0,0 +1,77 @@
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+GOOD LUCK 😀
+*/
+
+// TEST DATA 1
+let weightMark = 78;
+let heightMark = 1.69;
+let BMIMark = weightMark / heightMark ** 2;
+
+let weightJohn = 92;
+let heightJohn = 1.95;
+let BMIJohn = weightJohn / heightJohn ** 2;
+
+console.log('1) Check if BMI Marks is higher than Mark?');
+console.log('BMI Mark is ' + BMIMark);
+console.log('BMI John is ' + BMIJohn);
+markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+console.log('Mark has a higher BMI than John is ' + markHigherBMI + '.');
+
+console.log('----------------------------------------------------------------');
+console.log('\n');
+
+// TEST DATA 2
+weightMark = 95;
+heightMark = 1.88;
+BMIMark = weightMark / heightMark ** 2;
+
+weightJohn = 85;
+heightJohn = 1.76;
+BMIJohn = weightJohn / heightJohn ** 2;
+
+console.log('2) Check if BMI Marks is higher than Mark?');
+console.log('BMI Mark is ' + BMIMark);
+console.log('BMI John is ' + BMIJohn);
+markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+console.log('Mark has a higher BMI than John is ' + markHigherBMI + '.');
+
+console.log('----------------------------------------------------------------');
+console.log('\n');
+
+// Function for compare BMIs
+function compareBMIs(markWeight, markHeight, johnWeight, johnHeight) {
+	// Calculate BMIs
+	let markBMI = markWeight / (markHeight * markHeight);
+	let johnBMI = johnWeight / (johnHeight * johnHeight);
+
+	// Compare BMIs
+	let markHigherBMI = markBMI > johnBMI;
+
+	// Output result
+	if (markHigherBMI) {
+		console.log('Mark has a higher BMI than John.');
+	} else {
+		console.log('John has a higher BMI than Mark.');
+	}
+}
+
+// test with test data
+compareBMIs(78, 1.69, 92, 1.95);
+
+console.log('----------------------------------------------------------------');
+console.log('\n');
+
+compareBMIs(95, 1.88, 85, 1.76);
+
+console.log('----------------------------------------------------------------');
+console.log('\n');
